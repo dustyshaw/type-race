6dd031d49f84b55a05b978cef16d7fbc5c239a7f
Dusty Shaw <shustydaw@gmail.com>
2025-03-20 21:28:45 -0600

    Got browsers to share state

---
+++ client/Data/TypeHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace client.Data
+{
+    public class TypeHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- client/Program.cs
+++ client/Program.cs
@@ -1,18 +1,28 @@
 using client.Components;
+using client.Data;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseResponseCompression();
+app.MapHub<TypeHub>("/typehub");
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
